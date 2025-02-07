a4f63573db77c1da7d696b48ac5ae9f8e8c84523
Dung Nguyen <dung.nguyen1@globee.hk>
2025-02-07 04:58:47 +0400

    a

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	counter := 0
+	for a := 1000; a <= 9999; a++ {
+		for b := a; b <= 9999; b++ {
+			s := fmt.Sprintf("%d", a*b)
+			if s[0] == s[len(s)-1] {
+				counter++
+			}
+		}
+	}
+	println(counter)
+}
