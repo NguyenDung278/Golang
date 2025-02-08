957c47fb4562c603b14fd75d934cb57e55232d62
Dung Nguyen <dung.nguyen1@globee.hk>
2025-02-08 15:16:21 +0400

    abc

--- main.go
+++ main.go
@@ -3,16 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	counter2 := 1
+	counter3 := 10
 	for a := 1000; a <= 99929; a++ {
 		for b := a; b <= 9999; b++ {
 			s := fmt.Sprintf("%d", a*b)
 			if s[0] == s[len(s)-1] {
-				counter2++
+				counter3++
 			}
 		}
 	}
-	println(counter2)
-	fmt.Println(" 12121212wo212r122232222ld12")
-	fmt.Println(" 212w121o212r12122232222ld12")
+	fmt.Println(" 212w121o21212212r12122232222ld12")
+
+	println(counter3)
+	fmt.Println(" 212w121o21212212r12122232222ld12")
+	println(counter3)
+	fmt.Println(10)
+
+	fmt.Println(" 1212121121212wo212r122232222ld12")
+	fmt.Println(" 212w121o21212212r12122232222ld12")
 }
